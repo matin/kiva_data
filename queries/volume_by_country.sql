900bf56f0e619bcdc2e5ef16d77e10301311c577
Matin Tamizi <mtamizi@gmail.com>
2017-07-26 13:04:46 -0700

    formating

--- queries/volume_by_country.sql
+++ queries/volume_by_country.sql
@@ -1,27 +1,27 @@
 WITH country_loans AS (
-  SELECT
-    country,
-    sum(funded_amount) funded_amount,
-    count(*) total,
-    avg(funded_amount) average
-  FROM loans
-  WHERE
-    funded_date BETWEEN '2016-01-01' and '2016-12-31' AND
-    status = 'funded'
-  GROUP BY country
+    SELECT
+      country,
+      sum(funded_amount) funded_amount,
+      count(*)           total,
+      avg(funded_amount) average
+    FROM loans
+    WHERE
+      funded_date BETWEEN '2016-01-01' AND '2016-12-31' AND
+      status = 'funded'
+    GROUP BY country
 ), total AS (
-  SELECT
-    sum(funded_amount) funded_amount,
-    sum(total) total
-  FROM country_loans
+    SELECT
+      sum(funded_amount) funded_amount,
+      sum(total)         total
+    FROM country_loans
 )
 SELECT
   cl.country,
-  cl.funded_amount::money,
+  cl.funded_amount :: MONEY,
   cl.total,
-  cl.average::money,
+  cl.average :: MONEY,
   (cl.funded_amount / t.funded_amount) percent_of_funded_amount,
-  (cl.total / t.total) percent_of_total
+  (cl.total / t.total)                 percent_of_total
 FROM country_loans cl, total t
 ORDER BY cl.total DESC
 ;
--- queries/volume_by_partner.sql
+++ queries/volume_by_partner.sql
@@ -1,32 +1,32 @@
 WITH partner_loans AS (
-  SELECT
-    p.id partner_id,
-    p.name partner_name,
-    l.country,
-    sum(funded_amount) funded_amount,
-    count(*) total,
-    avg(funded_amount) average
-  FROM loans l
-  JOIN partners p ON p.id = l.partner_id
-  WHERE
-    l.funded_date BETWEEN '2016-01-01' and '2016-12-31' AND
-    l.status = 'funded'
-  GROUP BY p.id, p.name, l.country
+    SELECT
+      p.id               partner_id,
+      p.name             partner_name,
+      l.country,
+      sum(funded_amount) funded_amount,
+      count(*)           total,
+      avg(funded_amount) average
+    FROM loans l
+      JOIN partners p ON p.id = l.partner_id
+    WHERE
+      l.funded_date BETWEEN '2016-01-01' AND '2016-12-31' AND
+      l.status = 'funded'
+    GROUP BY p.id, p.name, l.country
 ), total AS (
-  SELECT
-    sum(funded_amount) funded_amount,
-    sum(total) total
-  FROM partner_loans
+    SELECT
+      sum(funded_amount) funded_amount,
+      sum(total)         total
+    FROM partner_loans
 )
 SELECT
   pl.partner_id,
   pl.partner_name,
   pl.country,
-  pl.funded_amount::money,
+  pl.funded_amount :: MONEY,
   pl.total,
   pl.average,
   (pl.funded_amount / t.funded_amount) percent_of_funded_amount,
-  (pl.total / t.total) percent_of_total
+  (pl.total / t.total)                 percent_of_total
 FROM partner_loans pl, total t
 ORDER BY pl.total DESC
 ;
