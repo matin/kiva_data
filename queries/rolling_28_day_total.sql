3e9020e58c38bfbfa42713ded060aacc9e26cfe4
Matin Tamizi <mtamizi@gmail.com>
2017-08-10 09:17:56 -0700

    formating

--- queries/rolling_28_day_total.sql
+++ queries/rolling_28_day_total.sql
@@ -1,31 +1,38 @@
 WITH days AS (
-  SELECT generate_series('2005-03-31'::date, '2017-07-21'::date, '1 day')::date AS day
+    SELECT generate_series('2005-03-31' :: DATE, '2017-07-21' :: DATE, '1 day') :: DATE AS day
 ), daily_funded_loans AS (
-  SELECT
-    CASE
-      WHEN funded_date < '2006-04-19' THEN posted_date::date
-          ELSE funded_date::date
-    END funded_on,
-    count(*) number_funded,
-    sum(funded_amount) amount_funded
-  FROM loans
-  WHERE status = 'funded'
-  GROUP BY funded_on
+    SELECT
+      CASE
+      WHEN funded_date < '2006-04-19'
+        THEN posted_date :: DATE
+      ELSE funded_date :: DATE
+      END                funded_on,
+      count(*)           number_funded,
+      sum(funded_amount) amount_funded
+    FROM loans
+    WHERE status = 'funded'
+    GROUP BY funded_on
 ),
-daily_total AS (
-  SELECT
-    day,
-    number_funded,
-    amount_funded
-  FROM days
-  LEFT JOIN daily_funded_loans ON funded_on = day
-)
+    daily_total AS (
+      SELECT
+        day,
+        number_funded,
+        amount_funded
+      FROM days
+        LEFT JOIN daily_funded_loans ON funded_on = day
+  )
 SELECT
   day,
-  sum(number_funded) OVER
-    (ORDER BY day ROWS BETWEEN 27 PRECEDING AND CURRENT ROW) number_funded,
-  sum(amount_funded) OVER
-    (ORDER BY day ROWS BETWEEN 27 PRECEDING AND CURRENT ROW)::money amount_funded
+  sum(number_funded)
+  OVER
+    (
+    ORDER BY day
+    ROWS BETWEEN 27 PRECEDING AND CURRENT ROW )          number_funded,
+  sum(amount_funded)
+  OVER
+    (
+    ORDER BY day
+    ROWS BETWEEN 27 PRECEDING AND CURRENT ROW ) :: MONEY amount_funded
 FROM daily_total
 ORDER BY day
 ;
