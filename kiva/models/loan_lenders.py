5b74da4a9604f58b2c5f01e858bf68d64afd35b2
Matin Tamizi <mtamizi@gmail.com>
2017-08-10 16:37:59 -0700

    fix dup problem

--- kiva/models/loan_lenders.py
+++ kiva/models/loan_lenders.py
@@ -11,7 +11,7 @@ class LoanLender(Base):
 
     @classmethod
     def transform(cls, loan_lenders) -> list:
-        lender_ids = loan_lenders['lender_ids'] or []
+        lender_ids = set(loan_lenders['lender_ids'] or [])
         return [
             LoanLender(loan_id=loan_lenders['loan_id'], lender_id=lender_id)
             for lender_id in lender_ids]
